1b791a7b0fed2dca4883d8e1f1a00e6561c1cb97
Alexander Solovyov <alexander@solovyov.net>
2017-11-24 16:38:38 +0200

    2.2.0

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject keybind "2.1.0"
+(defproject keybind "2.2.0"
   :description "ClojureScript key bindings (shortcut) library"
   :url "https://github.com/piranha/keybind"
   :scm {:name "git" :url "https://github.com/piranha/keybind"}
   :license {:name "ISC License"
             :url "http://www.isc.org/downloads/software-support-policy/isc-license/"}
   :plugins [[lein-cljsbuild "1.1.6"]
-            [lein-doo "0.1.7"]]
+            [lein-doo "0.1.8"]]
 
   :profiles
   {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
-                        [org.clojure/clojurescript "1.9.854" :scope "test"]]}}
+                        [org.clojure/clojurescript "1.9.946" :scope "test"]]}}
 
   :cljsbuild
   {:builds [{:id "test"
